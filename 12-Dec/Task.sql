b647e6ea14288da6c7f13f6578e0de6b158760a2
RoginaIrshaidat <115452721+RoginaIrshaidat@users.noreply.github.com>
2022-12-13 05:39:10 -0800

    Update Task.sql

--- 12-Dec/Task.sql
+++ 12-Dec/Task.sql
@@ -1,11 +1,28 @@
-create table customers (
+create table customer (
 Id int primary key,
 Name varchar(20)not null,
 phone int,
 );
 
-create table orders (
+
+create table order1 (
 Id int primary key,
-price double,
+price int,
 numOfProduct  int,
+orderDate date,
+);
+
+
+create table Product (
+Id int primary key,
+price float,
+nameOfProduct char(30),
+);
+
+create table Employee (
+Id int primary key,
+fName char (20) not null,
+lName char(20),
+salary int,
+numOfHour int not null,
 );
